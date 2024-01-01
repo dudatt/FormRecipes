7ffe3024183fc9745c7a8ca4fb7d1590d83e45eb
Maria Eduarda <dudamelobarbosa@gmail.com>
2024-01-01 14:22:28 -0300

    ok

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+let tema = document.getElementById('moon_icon');
+
+tema.addEventListener('click', () => {
+    const container = document.getElementById('container');
+    const form = document.getElementById('login_esp');
+    if (tema.classList.contains('fa-moon')){
+        tema.classList.remove('fa-moon');
+        tema.classList.add('fa-sun');
+
+        container.classList.add('dark');
+        form.classList.add('dark');
+        return
+    }
+    tema.classList.add('fa-moon');
+    tema.classList.remove('fa-sun');
+    container.classList.remove('dark');
+    form.classList.remove('dark');
+})
+
